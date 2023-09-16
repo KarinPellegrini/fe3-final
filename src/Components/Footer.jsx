8e0ca764d35a5c96dfe9e988374b7e518d1d86d7
KarinPellegrini <karinfarnholc@gmail.com>
2023-09-16 11:10:48 -0300

    corrigiendo rutas relativas

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,13 +4,13 @@ const Footer = () => {
   return (
     <footer>
       <p>Powered by</p>
-      <img src="public/images/DH.png" alt='DH-logo' className='logo' />
+      <img src="../../public/images/DH.png" alt='DH-logo' className='logo' />
 
       <div className='redes'>
-        <img src="public/images/ico-facebook.png" alt="Facebook" />
-        <img src="public/images/ico-instagram.png" alt="Instagram" />
-        <img src="public/images/ico-tiktok.png" alt="TikTok" />
-        <img src="public/images/ico-whatsapp.png" alt="WhatsApp" />
+        <img src="../../public/images/ico-facebook.png" alt="Facebook" />
+        <img src="../../public/images/ico-instagram.png" alt="Instagram" />
+        <img src="../../public/images/ico-tiktok.png" alt="TikTok" />
+        <img src="../../public/images/ico-whatsapp.png" alt="WhatsApp" />
       </div>
     </footer>
   )
