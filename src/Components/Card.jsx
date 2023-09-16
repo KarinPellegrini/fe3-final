4a04f466ea6c0c78ab16ad7cf5e6d00d6da95725
KarinPellegrini <karinfarnholc@gmail.com>
2023-09-16 02:07:25 -0300

    agregando themes en cards

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,29 +4,29 @@ import { useCharStates } from "./utils/Context";
 
 const Card = ({ char }) => {
 
-  const { dispatch } = useCharStates()
+  const { dispatch, state } = useCharStates()
 
 
   const addFav = () => {
-    // Aqui iria la logica para agregar la Card en el localStorage
     dispatch({ type: 'ADD_FAV', payload: char })
   }
 
+  const linkStyles = {
+    color: state.theme ? 'white' : 'black', // Cambia el color del texto del enlace
+    textDecoration: 'none', // Quita la subrayado predeterminado de los enlaces
+  };
+
   return (
     <div>
-      <div className="card">
+      <div className="card" >
         {/* En cada card deberan mostrar en name - username y el id */}
-        <Link to={'/detail/' + char.id}>
+        <Link to={'/detail/' + char.id} style={linkStyles}>
           <img src='public/images/doctor.jpg' alt="Foto doctor" />
           <h3>{char.name}</h3>
           <h4>{char.username}</h4>
           <h5>{char.id}</h5>
         </Link>
 
-        {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
-
-        {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-        {/* <button onClick={addFav} className="favButton">⭐</button> */}
         <button onClick={addFav} className="favButton">⭐</button>
       </div>
     </div>
